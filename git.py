b58741ec0fbd56c278d3be581da0d9bb372b12b0
LAXIT JANGID <139094932+Laxit85@users.noreply.github.com>
2025-06-13 21:15:21 +0530

    Updated git.py with new code from Desktop

--- git.py
+++ git.py
@@ -1,5 +1,21 @@
+
+# right-angled triangle pattern
 n = int(input())
 for i in range(n):
     for j in range(i+1):
         print("*",end=" ")
+    print()
+    
+    
+# horizontal pyramid pattern
+n1 = int(input()) 
+
+for i in range(n-1):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()  # Move to the next line
+    
+for i in range(n):
+    for j in range(n-i):
+        print("*", end=" ")
     print()
