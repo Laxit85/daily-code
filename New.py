6ef7fbf2d174c789e17d84c0b56d8363b5a074f3
LAXIT JANGID <139094932+Laxit85@users.noreply.github.com>
2025-06-16 22:27:42 +0530

    Updated New.py

---
+++ New.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+s = input()
+
+# Your code here
+words = s.split()
+capitalized_words = [word.capitalize() for word in words]
+capitalized_line = " ".join(capitalized_words)
+count = len(words)
+print(capitalized_line)
+print(count)
