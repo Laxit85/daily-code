7522d714381819edc0eee7fe8b46ae998d5b0ee6
LAXIT JANGID <139094932+Laxit85@users.noreply.github.com>
2025-06-18 00:41:25 +0530

    Updated New.py

--- New.py
+++ New.py
@@ -1,13 +1,25 @@
 #User function Template for python3
-s = input()
+#s = input()
 
 # Your code here
-words = s.split()
+#words = s.split()
 capitalized_words = [word.capitalize() for word in words]
 capitalized_line = " ".join(capitalized_words)
 count = len(words)
 print(capitalized_line)
 print(count)
 
+def alphabet_triangle(n):
+    for i in range(n):
+        for j in range(i+1):
+            print(chr(65 + j), end=' ')
+        print()
 
-hiiiiiiiiiiiiiiiiiiiiiiiiiii 
+#sum of eliments present in tuple
+
+def n_sum(*elements):
+    a = 0
+    for i in elements:
+        a += i
+    return a
+
