c43e3cf50794d50a4d52637570bd0d018992f116
LAXIT JANGID <139094932+Laxit85@users.noreply.github.com>
2025-06-18 23:40:31 +0530

    Updated git.py

--- git.py
+++ git.py
@@ -43,3 +43,20 @@ for i in range(n):
     
 for i in range(n):
     print(1,end=" ") 
+    
+    
+def solid_diamond_number(n):
+    # Upper part
+    for i in range(1, n+1):
+        for j in range(n - i):
+            print(' ', end='')
+        for k in range(2*i - 1):
+            print(i, end='')
+        print()
+    # Lower part
+    for i in range(n-1, 0, -1):
+        for j in range(n - i):
+            print(' ', end='')
+        for k in range(2*i - 1):
+            print(i, end='')
+        print()
